e93fb029a11a321050c715341edd35983e3c2b1a
anderuraga <ander.uraga@gmail.com>
2018-02-05 17:28:30 +0100

    servicio Heores Observable y MessageService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,15 +4,17 @@ import { of } from 'rxjs/observable/of';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 /* Acordarse de registrar en app.module.ts como 'provider' */
 
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]>{
+    this.messageService.add('Fetched Heroes');
     return of(HEROES);
   }
 
